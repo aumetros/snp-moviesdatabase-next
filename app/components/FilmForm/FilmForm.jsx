f11672a9176a077d9a421e812de513f456648efe
aumetros <aum3tros@yandex.ru>
2023-11-02 19:29:17 +0300

    Add Header: Реализована валидация формы

--- app/components/FilmForm/FilmForm.jsx
+++ app/components/FilmForm/FilmForm.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useForm } from "react-hook-form";
 import styles from "./FilmForm.module.scss";
 
@@ -6,54 +6,106 @@ export default function FilmForm({ title, name, submitText }) {
   const {
     register,
     handleSubmit,
-    formState: { errors },
-  } = useForm();
+    reset,
+    formState: { errors, isValid },
+  } = useForm({
+    mode: "onChange",
+  });
+
 
 
   return (
-    <form className={styles.root} name={name} onSubmit={handleSubmit((data) => console.log(data))}>
+    <form
+      className={styles.root}
+      name={name}
+      onSubmit={handleSubmit((data) => console.log(data))}
+    >
       <h2 className={styles.title}>{title}</h2>
       <hr className={styles.line} />
       <input
         className={styles.input}
         type="text"
-        name="filmTitle"
+        name="title"
+        maxLength={51}
         placeholder="Введите название фильма"
-        {...register("filmTitle")}
+        {...register("title", { 
+          required: "Название фильма обязательно к заполнению.",
+          minLength: {
+            value: 2,
+            message: "Название должно содержать минимум 2 символа."
+          },
+          maxLength: {
+            value: 50,
+            message: "Название фильма должно быть не длинее 50 символов."
+          }
+         })}
       />
-      <span className={styles.error}>Ошибка.</span>
+      <span className={styles.error}>
+        {errors?.title && errors?.title?.message}
+      </span>
 
       <input
         className={styles.input}
         type="text"
-        name="filmDirector"
+        name="director"
+        maxLength={41}
         placeholder="Введите имя режиссера фильма"
-        {...register("filmDirector")}
+        {...register("director", {
+          required: "Имя режиссера обязательно к заполнению.",
+          minLength: {
+            value: 3,
+            message: "Имя режиссера должно содержать не менее 3 символов."
+          },
+          maxLength: {
+            value: 40,
+            message: "Имя режиссера должно быть не длинее 40 символов."
+          }
+        })}
       />
-      <span className={styles.error}>Ошибка.</span>
+      <span className={styles.error}>
+        {errors?.director && errors?.director?.message}
+      </span>
 
       <input
         className={styles.input}
         type="text"
-        name="filmDate"
+        name="year"
+        maxLength={4}
         placeholder="Введите год создания фильма"
-        {...register("filmDate")}
+        {...register("year", {
+          required: "Год выхода фильма обязателен к заполнению.",
+          pattern: {
+            value: /^[12][0-9]{3}$/,
+            message: "Год выхода фильма должен быть в формате ГГГГ."
+          }
+        })}
       />
-      <span className={styles.error}>Ошибка.</span>
+      <span className={styles.error}>
+        {errors?.year && errors?.year?.message}
+      </span>
 
       <input
         className={styles.input}
         type="text"
-        name="filmPoster"
+        name="poster"
         placeholder="Введите ссылку на постер фильма"
-        {...register("filmPoster")}
+        {...register("poster", {
+          required: "Ссылка на постер фильма обязательна к заполнению.",
+          pattern: {
+            value: /(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?/g,
+            message: "Необходимо ввести ссылку.",
+          }
+        })}
       />
-      <span className={styles.error}>Ошибка.</span>
+      <span className={styles.error}>
+        {errors?.poster && errors?.poster?.message}
+      </span>
+
       <div className={styles["buttons-container"]}>
         <button
           type="submit"
-          className={`${styles.submit} ${false && styles.inactive}`}
-          disabled={false}
+          className={`${styles.submit} ${!isValid && styles.inactive}`}
+          disabled={!isValid}
         >
           {submitText}
         </button>
