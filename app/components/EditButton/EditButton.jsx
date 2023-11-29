ec841e3bb362855e71d4a45897324bba27014e56
aumetros <aum3tros@yandex.ru>
2023-11-29 18:53:42 +0300

    Add EditButton: Описана логика открытия окна редактирования

--- app/components/EditButton/EditButton.jsx
+++ app/components/EditButton/EditButton.jsx
@@ -1,8 +1,18 @@
+import { useDispatch } from "react-redux";
+import { openModal } from "store/slices/modalsSlice";
+import { setPreview } from "store/slices/filmsSlice";
 import Button from "components/Button";
 import styles from "./EditButton.module.scss";
 
-export default function EditButton() {
+export default function EditButton({ film }) {
+  const dispatch = useDispatch();
+
+  function handleEditClick() {
+    dispatch(setPreview(film));
+    dispatch(openModal("editFilm"));
+  }
+  
   return (
-    <Button className={styles.root} />
+    <Button className={styles.root} onClick={handleEditClick} />
   )
 }
--- app/components/Film/Film.jsx
+++ app/components/Film/Film.jsx
@@ -6,12 +6,12 @@ export default function Film({ film }) {
   return (
     <li className={styles.root}>
       <div className={styles.container}>
-      <div className={styles.bullet}></div>
-      <p className={styles.title}>{`"${film.title}" / (${film.year})`}</p>
+        <div className={styles.bullet}></div>
+        <p className={styles.title}>{`"${film.title}" / (${film.year})`}</p>
       </div>
       <div className={styles.actions}>
         <PreviewButton film={film} />
-        <EditButton />
+        <EditButton film={film} />
       </div>
     </li>
   );
