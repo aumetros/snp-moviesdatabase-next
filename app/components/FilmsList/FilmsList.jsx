9d0711a7428518cfe94022a12a4bd020b218b82d
aumetros <aum3tros@yandex.ru>
2023-12-11 23:05:09 +0300

    Add FilmList: Реализована фильтрация фильмов

--- app/components/FilmsList/FilmsList.jsx
+++ app/components/FilmsList/FilmsList.jsx
@@ -2,12 +2,17 @@
 import React from "react";
 import Film from "components/Film";
 import { useSelector, useDispatch } from "react-redux";
-import { selectFilms } from "store/selectors";
+import { selectFilms, selectFilter } from "store/selectors";
 import { setFilms } from "store/slices/filmsSlice";
 import styles from "./FilmsList.module.scss";
 
 export default function FilmsList() {
   const dispatch = useDispatch();
+  const films = useSelector(selectFilms);
+  const filter = useSelector(selectFilter);
+
+  const filterFilms = (filter) => (film) =>
+    film.title.toLowerCase().includes((filter).toLowerCase());
 
   React.useEffect(() => {
     if (localStorage.getItem("filmsState") !== null) {
@@ -15,10 +20,9 @@ export default function FilmsList() {
     }
   }, [dispatch]);
 
-  const films = useSelector(selectFilms);
   return (
     <ul className={styles.root}>
-      {films.map((film) => {
+      {films.filter(filterFilms(filter)).map((film) => {
         return <Film key={film.id} film={film} />;
       })}
     </ul>
