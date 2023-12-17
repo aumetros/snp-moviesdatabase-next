c03e4a87cb9d888612a25980f68dbf92efe55efa
aumetros <aum3tros@yandex.ru>
2023-12-18 00:26:45 +0300

    Fix Header: Реализована очистка поиска при возврате на гл страницу

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -2,16 +2,21 @@
 import { useRouter } from "next/navigation";
 import Counter from "components/Counter";
 import AddButton from "components/AddButton";
+import { useDispatch } from "react-redux";
+import { setFilter } from "store/slices/filtersSlice";
 import styles from "./Header.module.scss";
 
 export default function Header() {
   const router = useRouter();
+  const dispatch = useDispatch();
+
+  function handleLogoClick() {
+    router.push("/");
+    dispatch(setFilter(""));
+  }
   return (
     <header className={styles.root}>
-      <div
-        className={styles["logo-container"]}
-        onClick={() => router.push("/")}
-      >
+      <div className={styles["logo-container"]} onClick={handleLogoClick}>
         <div className={styles.logo}></div>
         <div className={styles["title-container"]}>
           <h1 className={styles.title}>Movies Database</h1>
